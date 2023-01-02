0499adc5827696e9647012440cc095c4eec219ac
Arius <70983185+Arius-Development@users.noreply.github.com>
2023-01-02 19:59:45 +0100

    Delete server directory

--- server/menu.lua
+++
@@ -1,8 +0,0 @@
-local ox_inventory = exports.ox_inventory
-
-
-lib.callback.register('ars_disassemble-weapons:getWeapons', function(source, weapon, w)
-    local items = exports.ox_inventory:GetInventoryItems(source)
-
-    return items
-end)
